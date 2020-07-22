d449d48bd1bdfe8e316ab42479dead4ac558b8cd
vagrant <vagrant@vagrant.vm>
2020-07-22 02:02:44 +0000

    DHRM2197 - first commit

---
+++ src/main/java/com/kingland/dhrm2197/Dhrm2197Application.java
@@ -0,0 +1,13 @@
+package com.kingland.dhrm2197;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dhrm2197Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dhrm2197Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kingland/dhrm2197/Dhrm2197ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingland.dhrm2197;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dhrm2197ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
