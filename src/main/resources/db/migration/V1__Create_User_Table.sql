91be4a588a07fc0f12a314d6ce703cc0f4f3cf22
gleli <897370429@qq.com>
2020-07-27 15:14:00 +0800

    Modify sql file.

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -7,3 +7,6 @@ CREATE TABLE public.users
     CONSTRAINT users_pkey PRIMARY KEY (uid),
     CONSTRAINT users_username_key UNIQUE (username)
 );
+
+INSERT INTO users VALUES (1, 'user', '$2a$10$DXf5nRS6Y2kV3ps2URGoZOdS/4eyDlsxoDah7ooyk5z06I2H7qH0i', 'USER');
+INSERT INTO users VALUES (2, 'admin', '$2a$10$2mChFxlZWLnA290gF4bcMuqSF9Q5/bWSjyxr8XBwLedjROwyYnmeC', 'ADMIN');
