83375850c0ef0a013f4d48e682882fc20695923b
gleli <897370429@qq.com>
2020-07-27 14:14:17 +0800

    fix bugs

--- src/main/java/com/kingland/dhrm2197/config/SecurityConfig.java
+++ src/main/java/com/kingland/dhrm2197/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+                .antMatchers("/register").permitAll()
                 .anyRequest()
                 .authenticated()
             .and()
